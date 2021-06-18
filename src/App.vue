e59fc66632afe07e40d0a4869cd08f48665232d9
Murat <murat199611@mail.ru>
2021-06-18 23:49:29 +0300

    add inputs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app">
+      <Form />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld/HelloWorld.vue';
+import Form from './components/Form/Form.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Form,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.body {
+  margin: 0;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,120 @@
+<template>
+  <form class="form">
+    <h1 class="form__title">
+      Регистрация
+    </h1>
+    <fieldset class="form__fieldset">
+      <legend>Личные данные</legend>
+      <input
+        type="text"
+        placeholder="Фамилия"
+        required
+      >
+      <input
+        type="text"
+        placeholder="Имя"
+        required
+      >
+      <input
+        type="text"
+        placeholder="Отчество"
+      >
+      <input
+        type="date"
+        placeholder="Дата рождения"
+        required
+      >
+      <input
+        type="tel"
+        placeholder="Номер телефона"
+        required
+      >
+      <input
+        type="text"
+        placeholder="Пол"
+      >
+      <select multiple>
+        <option>VIP</option>
+        <option>Проблемные</option>
+        <option>ОМС</option>
+      </select>
+      <select>
+        <option>Иванов</option>
+        <option>Захаров</option>
+        <option>Чернышева</option>
+      </select>
+      <input
+        type="checkbox"
+        placeholder="Не отправлять СМС"
+      >
+    </fieldset>
+    <fieldset class="form__fieldset">
+      <legend>Адрес</legend>
+      <input
+        type="number"
+        placeholder="Индекс"
+      >
+      <input
+        type="text"
+        placeholder="Страна"
+      >
+      <input
+        type="text"
+        placeholder="Область"
+      >
+      <input
+        type="text"
+        placeholder="Город"
+        required
+      >
+      <input
+        type="text"
+        placeholder="Улица"
+      >
+      <input
+        type="text"
+        placeholder="Дом"
+        required
+      >
+    </fieldset>
+    <fieldset class="form__fieldset">
+      <legend>Паспорт</legend>
+      <select>
+        <option>Паспорт</option>
+        <option>Свидетельство о рождении</option>
+        <option>Вод. удостоверение</option>
+      </select>
+      <input
+        type="number"
+        placeholder="Серия"
+      >
+      <input
+        type="number"
+        placeholder="Номер"
+      >
+      <input
+        type="text"
+        placeholder="Кем выдан"
+      >
+      <input
+        type="date"
+        placeholder="Дата выдачи"
+        required
+      >
+    </fieldset>
+  </form>
+</template>
+
+<style scoped lang="scss">
+@import "./Form.sass";
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Привfdgdfgет мир!',
+    };
+  },
+};
+</script>
